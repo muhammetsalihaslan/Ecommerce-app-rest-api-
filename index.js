624b9756dcf526467d0ccfbf1e1e687f6439ed61
salih <muhammetsalihaslan@gmail.com>
2022-11-15 22:17:41 +0300

    index

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello3")
+const express = require('express'); //its a import a exprees library 
+const app = express();
+const mongoose = require('mongoose'); //its a import mongoose library
+
+app.listen(5000, ()=>{
+    console.log("Backend server is running!")
+});
+

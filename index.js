2f85c30f3a7ce66945574414deabf03dbae492a3
salih <muhammetsalihaslan@gmail.com>
2022-11-16 12:00:18 +0300

    user thing

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express'); //its a import a exprees library 
 const app = express();
 const mongoose = require('mongoose'); //its a import mongoose library
+const dotenv = require('dotenv');// its a import dotenv 
+const userRoute = require('./routes/user');
 
-app.listen(5000, ()=>{
+dotenv.config(); //configuration is nedden othervise i cant useit
+
+mongoose.connect(process.env.MONGO_URL).then(() => console.log('DBconnection Succesfull')).catch((err) => {
+    console.log(err);
+});
+
+
+app.use("/api/user", userRoute); //whenever we go to api/user our application will use userRoute (code mean)
+
+//   app.get("/api/test", () => {
+//     console.log('test is successfull')
+//   })  //!create roots so i am creating rest api we are gonna use some endpoints  but here its not a good idea to write /api/test we sould to create routes folder
+
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Backend server is running!")
 });
 

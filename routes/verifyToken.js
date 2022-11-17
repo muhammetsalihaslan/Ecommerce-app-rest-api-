8ba310871275f6ce63716feec75ac7ce0379fa32
salih <muhammetsalihaslan@gmail.com>
2022-11-17 23:13:17 +0300

    verify

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -2,22 +2,23 @@ const jwt = require('jsonwebtoken')
 
 
 const verifyToken = (req,res,next) =>{
-    const authHeader = req.headers.token
+    const authHeader = req.headers.token;
     if(authHeader){
+        const token = authHeader.split(" ")[1];
        jwt.verify(token, process.env.JWT_SEC, (err, user) =>{
         if(err) res.status(403).json("Token is not valid!");
         req.user = user;
         next();
-       })
+       });
     }else{
         return res.status(401).json("you are not authenticated!")
     }
-}
+};
 
 
 
 const verifyTokenAndAuthorization = (req, res, next) => {
-    verifyToken(req,res, () =>{
+    verifyToken(req, res, () =>{
          if(req.user.id === req.params.id || req.user.isAdmin){
         next();
     } else{
